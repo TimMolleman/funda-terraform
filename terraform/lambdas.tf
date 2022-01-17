188db8628a79111927b8a8efd0210bb44a39679c
TimMolleman <timmolleman@hotmail.com>
2022-01-17 16:33:12 +0100

    Update

--- terraform/lambdas.tf
+++ terraform/lambdas.tf
@@ -1,20 +1,20 @@
 resource "aws_iam_role" "lambda_iam_role" {
   name = "lambda_iam_role"
   assume_role_policy = <<EOF
-  {
-    "Version": "2012-10-17",
-    "Statement": [
-      {
-        "Action": "sts:AssumeRole",
-        "Principal": {
-          "Service": "lambda.amazonaws.com"
-        },
-        "Effect": "Allow",
-        "Sid": ""
-      }
-    ]
-  }
-  EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
 }
 
 resource "aws_lambda_function" "funda-link-scraper" {
