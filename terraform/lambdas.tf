343b02fef623adcc26a0dd9dd3951ff2ad1f042e
TimMolleman <timmolleman@hotmail.com>
2022-01-17 17:05:00 +0100

    Update

--- terraform/lambdas.tf
+++ terraform/lambdas.tf
@@ -17,6 +17,11 @@ resource "aws_iam_role" "lambda_iam_role" {
 EOF
 }
 
+resource "aws_iam_policy_attachment" "lambda_basic_execution_policy" {
+  name       = aws_iam_role.lambda_iam_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
 resource "aws_lambda_function" "funda-link-scraper" {
   function_name = "funda-link-scraper"
   role = aws_iam_role.lambda_iam_role.arn
